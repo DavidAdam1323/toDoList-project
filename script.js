ab769819844231a0480cbb02a1ce0fdf9c0daeaa
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-18 23:44:35 +0100

    Implemented dynamic task display functionality using HTML elements and updated the task list view

--- script.js
+++ script.js
@@ -5,6 +5,9 @@
 const task = document.querySelector(".input-task");
 const button = document.querySelector(".addBtn");
 
+// Select the element with the class "list-tasks" and store it in the fullList variable; ✅
+const fullList = document.querySelector(".list-tasks");
+
 let listOfTasks = []; // Initialize an array to store the user's tasks; ✅
 
 // Function to add the value of the task input to the listOfTasks array; ✅
@@ -12,6 +15,30 @@ function addingTask() {
   listOfTasks.push(task.value); // Add the value of the task input to the listOfTasks array; ✅
   task.value = ""; // Clear the input field after adding the task; ✅
   console.log(listOfTasks);
+
+  displayTask();
+}
+
+// Function to display tasks in the list; ✅
+function displayTask() {
+  let newTask = ""; // Initialize an empty string to store the new task HTML elements; ✅
+
+  // Iterate through each task in the listOfTasks array; ✅
+  listOfTasks.forEach((task) => {
+    // Create HTML elements for each task and append them to the newTask string; ✅
+    newTask =
+      newTask +
+      `
+  <li class="task">
+    <img src="./images/checked.png" alt="check-icon" />
+    <p>${task}</p>
+    <img src="./images/trash.png" alt="trash-icon" />
+  </li>
+  `;
+  });
+
+  // Update the content of the fullList element with the newTask string; ✅
+  fullList.innerHTML = newTask;
 }
 
 // Add an event listener to the button element that listens for a click event and calls the addingTask function when the button is clicked; ✅
